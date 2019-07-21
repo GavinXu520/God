3258c2f037adafed03e614ec995e7edb6a0ab3c0
gavin <meiyan532484710@163.com>
2019-07-21 11:57:44 +0800

    Signed-off-by: gavin <meiyan532484710@163.com>

--- core/main.go
+++ core/main.go
@@ -72,7 +72,7 @@ func SetupServer() {
 		log.Printf("%s\n\n%s\n", banner, fmt.Sprintf("%s: Running at %s", time.Now().Format("Mon, 02 Jan 2006 15:04:05 GMT"), port))
 		// server connections
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+			log.Fatalf("Failed to listen: %s\n", err)
 		} else {
 			log.Printf("%s\n\n%s\n", "Good bye, see you next time !!", fmt.Sprintf("%s: Running at %s", time.Now().Format("Mon, 02 Jan 2006 15:04:05 GMT"), port))
 		}
